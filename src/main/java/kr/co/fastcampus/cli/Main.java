471f79898dcc1ec967b6ad31908c5f1f99beb64b
khm0813 <khm0813@modernwave.co.kr>
2020-03-12 10:17:29 +0900

    패스트캠퍼스 강좌 Java-based Container Configuration 종료

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -3,17 +3,21 @@ package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Slf4j
 class Main {
 	public static void main(String[] ar) throws Exception {
-//		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("__dao.xml");
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		context.scan("kr.co.fastcampus.cli");
-		context.refresh();
-		val b = context.getBean(B.class);
-		log.info(""+b);
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//		context.scan("kr.co.fastcampus.cli");
+//		context.refresh();
+//		val b = context.getBean(B.class);
+//		log.info(""+b);
+		val dao = context.getBean(Dao.class);
+		dao.run();
 		context.close();
 
 	}
